2be3c073d99e426a6a26c6e15a1a4448440f9fe0
Rishav Kumar <73604171+Rishav00014@users.noreply.github.com>
2024-08-23 15:14:17 +0530

    Update config.js

--- humile-backend/config.js
+++ humile-backend/config.js
@@ -13,7 +13,7 @@ module.exports = {
   JWT_SECRET: "myjwt$#122345secretkey",
 
   //baseURL
-  baseURL: "http://localhost:5000",
+  baseURL: "http://134.209.149.35:5000",
 
   //Firebase server key for send notification through firebase
   SERVER_KEY: "BIMJabS9boYekntGU4aaO4Ux2X4wHsfUlay6XYzn2BdZQmzClkA-wjiOah2Clg5wjQqTK22XeYV8KBFWRG677Ic",
